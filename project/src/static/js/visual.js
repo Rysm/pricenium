cb5065b56a4ab3ee32f0c552e304f0630dd456ed
Andy Wang <awan26@ucsc.edu>
2017-08-01 22:35:50 -0700

    wip frontend

--- project/src/static/js/visual.js
+++ project/src/static/js/visual.js
@@ -4,32 +4,74 @@ Javascript code that handles all the chart stuff for the front end
 
 */
 
-queue()
-	.defer( d3.json, "/pricenium/posts"  )
-	//.defer( d3.json, "../../json/data.json" )
-	.await(makeGraphs);
 
-//The main graph making function
-function makeGraphs(error, projectJSON){
+InitChart();
 
-	var dbData = projectJSON;
+function InitChart() {
 
-	console.log(dbData);
+  var lineData = [{
+    'x': 1,
+    'y': 5
+  }, {
+    'x': 20,
+    'y': 20
+  }, {
+    'x': 40,
+    'y': 10
+  }, {
+    'x': 60,
+    'y': 40
+  }, {
+    'x': 80,
+    'y': 5
+  }, {
+    'x': 100,
+    'y': 60
+  }];
 
-	var timeChart = dc.barChart("#time-chart");
+  var vis = d3.select("#price-chart"),
+    WIDTH = 700,
+    HEIGHT = 500,
+    MARGINS = {
+      top: 20,
+      right: 20,
+      bottom: 20,
+      left: 50
+    },
+    xRange = d3.scaleLinear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([d3.min(lineData, function (d) {
+        return d.x;
+      }),
+      d3.max(lineData, function (d) {
+        return d.x;
+      })
+    ]),
 
-	timeChart
-	    .width(600)
-	    .height(160)
-	    .margins({top: 10, right: 50, bottom: 30, left: 50})
-	    .dimension(dateDim)
-	    .group(numProjectsByDate)
-	    .transitionDuration(500)
-	    .x(d3.time.scale().domain([minDate, maxDate]))
-	    .elasticY(true)
-	    .xAxisLabel("Days")
-	    .yAxis().ticks(4);
+    yRange = d3.scaleLinear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([d3.min(lineData, function (d) {
+        return d.y;
+      }),
+      d3.max(lineData, function (d) {
+        return d.y;
+      })
+    ]),
+
+    xAxis = d3.axisBottom()
+      .scale(xRange)
+      .tickSize(5)
+
+    yAxis = d3.axisLeft()
+      .scale(yRange)
+      .tickSize(5)
 
 
-	dc.renderAll();
+  vis.append("svg:g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + (HEIGHT - MARGINS.bottom) + ")")
+    .call(xAxis);
+
+  vis.append("svg:g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+    .call(yAxis);
+
+
 }
