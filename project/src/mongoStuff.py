8be5c125681745832337d21a56445d5ac363bd65
Andy Wang <awan26@ucsc.edu>
2017-08-04 12:30:44 -0700

    Mongodb now taking multiple dictionaris to each collection

--- project/src/mongoStuff.py
+++ project/src/mongoStuff.py
@@ -1,49 +1,62 @@
 #Gets the data from the price grabber py and inserts into mongodb
+#rip coding practices
 
 from pymongo import MongoClient
 import json
-import re
+import re #use the python raw_decoder to correctly handle multiple data
 
+#Specify URL and port
 client = MongoClient( "localhost", 27017)
 
+#Our target db client
 db = client["pricenium"]
 
+#Declare the collections I used
 posts = db.posts #contains the dictionary of item names as keys and date/price as values
-
 names = db.names #contains list of items that can match the keys
-
 stamps = db.stamps #contains list of items that can match the keys
 
-#read the data json from pricescraper
-selData1 = open("json/data.json", "r")
+#A raw_decode example by falsetru
+#https://stackoverflow.com/a/22112491/5627221
+nonspace = re.compile(r"\S")
+def iterparse(j):
+    decoder = json.JSONDecoder()
+    pos = 0
+    while True:
+        matched = nonspace.search(j, pos)
+        if not matched:
+            break
+        pos = matched.start()
+        decoded, pos = decoder.raw_decode(j, pos)
+        yield decoded
 
-#Parse the data
-parseData1 = json.loads( selData1.read() )
+####### Everything below this line is data  #######
 
+#Clear it out first because I keep altering the database structure
 posts.remove()
-
-posts.insert(parseData1,check_keys=False)
-
-####### KEK #######
-
-#read the data json from pricescraper
-selData2 = open("json/names.json", "r")
-
-#Parse the data
-parseData2 = json.loads( selData2.read() )
-
 names.remove()
-
-names.insert(parseData2,check_keys=False)
-
-####### KEK #######
-
-#read the data json from pricescraper
-selData3 = open("json/stamps.json", "r")
-
-#Parse the data
-parseData3 = json.loads( selData3.read() )
-
 stamps.remove()
 
-stamps.insert(parseData3,check_keys=False)
+#read the data json
+sel1 = open("json/data.json", "r")
+
+for piece in list(iterparse(sel1.read())):
+
+    posts.insert( piece , check_keys=False)
+
+
+
+#read the item names
+sel2 = open("json/names.json", "r")
+
+for piece in list(iterparse(sel2.read())):
+
+    names.insert( piece , check_keys=False)
+
+
+#read the time stamps from pricescraper
+sel3 = open("json/stamps.json", "r")
+
+for piece in list(iterparse(sel3.read())):
+
+    stamps.insert( piece , check_keys=False)
